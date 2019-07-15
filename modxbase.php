7fd9e583f1b30e06f40263594c194285351f0ac9
unknown <rafrhodes@uahoo.co.uk>
2019-07-15 13:48:46 +0100

    tidy up

---
+++ modxbase.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Connected Photography</title>
+</head>
+
+?<?php 
+    <body>
+        <div class="preloader-text">
+        <div class="text-wrap">CP</div>
+        <span>loading...</span>
+    </div>
+
+    <div class="main-wrapper footer-parallax" data-sound="/audio" data-top="992">
+
+        [[*common_header]]
+
+    </div>
+
+ ?>
+
+    
+</body>
+</html>
